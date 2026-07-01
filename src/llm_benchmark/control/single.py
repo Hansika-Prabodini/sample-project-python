e9b341a4bd786590f82dddcf50715b3b0c44a7cc
Artemis Git Integration <artemis@turintech.ai>
2026-07-01 10:10:17 +0000

    replace modulus sum loop with formula

--- src/llm_benchmark/control/single.py
+++ src/llm_benchmark/control/single.py
@@ -41,8 +41,5 @@ class SingleForLoop:
         Returns:
             int: Sum of modulus of numbers from 0 to n
         """
-        arr = []
-        for i in range(n):
-            if i % m == 0:
-                arr.append(i)
-        return sum(arr)
+        max_multiple = (n - 1) // m
+        return m * max_multiple * (max_multiple + 1) // 2
