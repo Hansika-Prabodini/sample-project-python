4d84fcb44014bb5b1fb2db9855284f12e0e519f7
Artemis Git Integration <artemis@turintech.ai>
2026-09-02 06:10:12 +0000

    bound prime checks by integer square root

--- src/llm_benchmark/algorithms/primes.py
+++ src/llm_benchmark/algorithms/primes.py
@@ -33,9 +33,7 @@ class Primes:
         if n < 2:
             return False
 
-        for i in range(2, n // 2 + 1):
-            if i > n // i:
-                break
+        for i in range(2, isqrt(n) + 1):
             if n % i == 0:
                 return False
 
