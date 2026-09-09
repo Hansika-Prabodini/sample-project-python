bf2c9570a506476c3331c5476fbfd6dc71abcbe3
Artemis Git Integration <artemis@turintech.ai>
2026-09-09 05:44:17 +0000

    bind album title in SQL query

--- src/llm_benchmark/sql/query.py
+++ src/llm_benchmark/sql/query.py
@@ -17,7 +17,7 @@ class SqlQuery:
         try:
             cur = conn.cursor()
 
-            cur.execute(f"SELECT * FROM Album WHERE Title = '{name}'")
+            cur.execute("SELECT * FROM Album WHERE Title = ?", (name,))
             return len(cur.fetchall()) > 0
         finally:
             conn.close()
